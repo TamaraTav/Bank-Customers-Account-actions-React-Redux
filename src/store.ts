04d66178b16a6381db0437a363f648d951b79b93
TamaraTav <tamara.tavamaishvili@gmail.com>
2026-01-06 15:12:00 -0600

    bug fix: legacy_createdStore and eslint-disable comment

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import custumerReducer from "./features/customers/customerSlice";
 import { thunk } from "redux-thunk";
+import type { ThunkDispatch } from "redux-thunk";
+import type { AnyAction } from "redux";
 
 const rootReducer = combineReducers({
   account: accountReducer,
@@ -10,8 +16,12 @@ const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  rootReducer as any,
+  applyMiddleware(thunk)
+);
 
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>;
 
 export default store;
