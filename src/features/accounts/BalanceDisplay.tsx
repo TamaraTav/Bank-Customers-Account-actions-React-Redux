5a5ff9bf6b554aa14cb9f35e68cd06bfec98f559
TamaraTav <tamara.tavamaishvili@gmail.com>
2026-01-06 14:45:56 -0600

    add Loan and loan purpose information in UI

--- src/features/accounts/BalanceDisplay.tsx
+++ src/features/accounts/BalanceDisplay.tsx
@@ -9,6 +9,18 @@ function formatCurrency(value: number) {
 }
 
 export default function BalanceDisplay() {
-  const balance = useSelector((store: RootState) => store.account.balance);
-  return <h1>{formatCurrency(balance)}</h1>;
+  const { balance, loan, loanPurpose } = useSelector(
+    (store: RootState) => store.account
+  );
+
+  return (
+    <div>
+      <h1>Balance: {formatCurrency(balance)}</h1>
+      {loan > 0 && (
+        <h2>
+          Loan: {formatCurrency(loan)} (Purpose: {loanPurpose})
+        </h2>
+      )}
+    </div>
+  );
 }
